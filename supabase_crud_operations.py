809f7a252c454e1cc31ae8ca07b0be3e4902dd70
Robert Soroka <74800762+RobbS79@users.noreply.github.com>
2024-07-05 14:40:13 +0200

    Update supabase_crud_operations.py
    Editting imports

--- supabase_crud_operations.py
+++ supabase_crud_operations.py
@@ -1,9 +1,7 @@
 from excel_supabase_crud_operations import ExcelSupabaseDataPipeline
-
-
-operator = ExcelSupabaseDataPipeline(excel_table_filepath="/Users/rob/Downloads/data_migration_softapp(motory)-2.csv",table_name="motory")
 from supabase_client import ConnectSupabaseInstance
 
+operator = ExcelSupabaseDataPipeline(excel_table_filepath="/Users/rob/Downloads/data_migration_softapp(motory)-2.csv",table_name="motory")
 operator.insert_rows_to_supabase_table()
 
 #table_to_import = pd.read_csv("/Users/rob/Downloads/data_migration_softapp(List1)-4.csv",encoding="cp852",sep=";")
